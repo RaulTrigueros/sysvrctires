d84a1214fdd636036a0b47fcf55ff6e0a5aff919
unknown <Rulodev@INFORMATICADEV.amsp.tacachico>
2025-02-04 14:14:43 -0600

    actualizacion de dependencias alert

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,4 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+
 import DateFilter from './filtros/date'
 
 require('./bootstrap');
@@ -11,14 +7,11 @@ window.Vue = require('vue');
 
 import VueFormWizard from 'vue-form-wizard'
 import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+import Swal from 'sweetalert2'
+Vue.prototype.$Swal = Swal
+
+
 Vue.use(VueFormWizard)
-/**
-
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('pedido', require('./components/Pedido.vue').default);
 Vue.component('cliente', require('./components/Cliente.vue').default);
