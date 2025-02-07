879f972ae0fe23b42abbbe7b83cb7ad964513657
unknown <Rulodev@INFORMATICADEV.amsp.tacachico>
2025-02-07 11:53:12 -0600

    actualizacion de dependencias alerta6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\User;
 use App\Persona;
-use Hash; 
+use Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate \ Support \ Facades \ Auth;
+use Illuminate\Support\Facades\Auth;
 use App\Services\BitacoraService;
 
 class UserController extends Controller
@@ -26,20 +26,19 @@ class UserController extends Controller
 
         $buscar = $request->buscar;
         $criterio = $request->criterio;
-        
-        if ($buscar==''){
-            $personas = User::join('personas','users.id','=','personas.id')
-            ->join('roles','users.idrol','=','roles.id')
-            ->select('personas.id','personas.nombre','personas.telefono','personas.email','users.cargo','users.usuario','users.password','users.condicion','users.idrol','roles.nombre as rol')
-            ->orderBy('personas.id', 'desc')->paginate(3);
+
+        if ($buscar == '') {
+            $personas = User::join('personas', 'users.id', '=', 'personas.id')
+                ->join('roles', 'users.idrol', '=', 'roles.id')
+                ->select('personas.id', 'personas.nombre', 'personas.telefono', 'personas.email', 'users.cargo', 'users.usuario', 'users.password', 'users.condicion', 'users.idrol', 'roles.nombre as rol')
+                ->orderBy('personas.id', 'desc')->paginate(3);
+        } else {
+            $personas = User::join('personas', 'users.id', '=', 'personas.id')
+                ->join('roles', 'users.idrol', '=', 'roles.id')
+                ->select('personas.id', 'personas.nombre', 'personas.telefono', 'personas.email', 'users.cargo', 'users.usuario', 'users.password', 'users.condicion', 'users.idrol', 'roles.nombre as rol')
+                ->where('personas.' . $criterio, 'like', '%' . $buscar . '%')->orderBy('id', 'desc')->paginate(3);
         }
-        else{
-            $personas = User::join('personas','users.id','=','personas.id')
-            ->join('roles','users.idrol','=','roles.id')
-            ->select('personas.id','personas.nombre','personas.telefono','personas.email','users.cargo','users.usuario','users.password','users.condicion','users.idrol','roles.nombre as rol')
-            ->where('personas.'.$criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(3);
-        }
-        
+
         return [
             'pagination' => [
                 'total'        => $personas->total(),
@@ -57,7 +56,7 @@ class UserController extends Controller
     {
         if (!$request->ajax()) return redirect('/');
 
-        try{
+        try {
             DB::beginTransaction();
 
             $persona = new Persona();
@@ -71,12 +70,12 @@ class UserController extends Controller
             $user->cargo = $request->cargo;
             $user->idrol = $request->idrol;
             $user->usuario = $request->usuario;
-            $user->password = bcrypt( $request->password);
-            $user->condicion = '1';            
+            $user->password = bcrypt($request->password);
+            $user->condicion = '1';
             $user->save();
 
             DB::commit();
-        } catch (Exception $e){
+        } catch (Exception $e) {
             DB::rollBack();
         }
 
@@ -88,7 +87,7 @@ class UserController extends Controller
     {
         if (!$request->ajax()) return redirect('/');
 
-        try{
+        try {
             DB::beginTransaction();
 
             $user = User::findOrFail($request->id);
@@ -99,13 +98,13 @@ class UserController extends Controller
             $persona->save();
             $user->cargo = $request->cargo;
             $user->usuario = $request->usuario;
-            $user->password = bcrypt( $request->password);
+            $user->password = bcrypt($request->password);
             $user->condicion = '1';
             $user->idrol = $request->idrol;
             $user->save();
 
             DB::commit();
-        } catch (Exception $e){
+        } catch (Exception $e) {
             DB::rollBack();
         }
 
@@ -135,46 +134,39 @@ class UserController extends Controller
         return "Éxito";
     }
 
-      //Modificar contraseña
-    public function password(){
+    //Modificar contraseña
+    public function password()
+    {
         return View('contenido/contrasena');
-       
     }
-    
-    public function updatePassword(Request $request){
+
+    public function updatePassword(Request $request)
+    {
         $rules = [
             'mypassword' => 'required',
             'password' => 'required|confirmed',
         ];
-        
+
         $messages = [
             'mypassword.required' => '**El campo es requerido**',
             'password.required' => '**El campo es requerido**',
             'password.confirmed' => '**Los passwords no coinciden**',
-//            'password.min' => 'El mínimo permitido son 6 caracteres',
-//            'password.max' => 'El máximo permitido son 18 caracteres',
+            //            'password.min' => 'El mínimo permitido son 6 caracteres',
+            //            'password.max' => 'El máximo permitido son 18 caracteres',
         ];
-        
+
         $validator = Validator::make($request->all(), $rules, $messages);
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return redirect('user/password')->withErrors($validator);
-        }
-        else{
-            if (Hash::check($request->mypassword, Auth::user()->password)){
+        } else {
+            if (Hash::check($request->mypassword, Auth::user()->password)) {
                 $user = new User;
                 $user->where('usuario', '=', Auth::user()->usuario)
-                     ->update(['password' => bcrypt($request->password)]);
-                return redirect('/main')->with('alert-success', 'La contraseña ha sido modificada Exitosamente!'); 
-                          
-            }
-            else
-            {
+                    ->update(['password' => bcrypt($request->password)]);
+                return redirect('/main')->with('alert-success', 'La contraseña ha sido modificada Exitosamente!');
+            } else {
                 return redirect('user/password')->with('message', 'Credenciales incorrectas');
             }
         }
-
-        
-        
     }
-
 }
